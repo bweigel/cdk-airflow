d2421ba81e6d695ddc0bea3ca70acd558d0e95d9
bweigel <benjamin.weigel@europace.de>
2019-08-03 00:21:24 +0200

    initial commit

---
+++ lib/airflow-docker.ts
@@ -0,0 +1,43 @@
+import codebuild = require('@aws-cdk/aws-codebuild');
+import ecr = require('@aws-cdk/aws-ecr');
+import cdk = require('@aws-cdk/core');
+import { PolicyStatement } from '@aws-cdk/aws-iam';
+
+
+
+export class AirflowDockerBuild extends cdk.Construct {
+    constructor(scope: cdk.Construct, id: string) {
+        super(scope, id);
+
+        const imageRepo = new ecr.Repository(this, "AirflowRepo", { repositoryName: 'airflow/base' })
+
+        const buildProject = new codebuild.Project(this, "AirflowDockerBuild", {
+            source: codebuild.Source.gitHub({ owner: 'bweigel', repo: 'cdk-airflow', webhook: true }),
+            badge: true,
+            environment: { buildImage: codebuild.LinuxBuildImage.UBUNTU_14_04_DOCKER_18_09_0 },
+            buildSpec: codebuild.BuildSpec.fromSourceFilename("lib/assets/docker-buildspec.yml"),
+            environmentVariables: {
+                ECR_QUALIFIED_REPO_NAME: { value: imageRepo.repositoryUri }
+            }
+        });
+
+        buildProject.addToRolePolicy(new PolicyStatement({
+            actions: [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:GetRepositoryPolicy",
+                "ecr:DescribeRepositories",
+                "ecr:ListImages",
+                "ecr:DescribeImages",
+                "ecr:BatchGetImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:PutImage"
+            ],
+            resources: [imageRepo.repositoryArn]
+        }))
+
+    }
+}
--- lib/cdk-airflow-stack.ts
+++ lib/cdk-airflow-stack.ts
@@ -1,18 +1,11 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import { AirflowDockerBuild } from './airflow-docker';
 
 export class CdkAirflowStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkAirflowQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    new AirflowDockerBuild(this, "DockerBuild");
 
-    const topic = new sns.Topic(this, 'CdkAirflowTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
